eab22de7eaddbb19431d5cc1fc8c8c193eba090a
spd <384220729@qq.com>
2024-10-16 10:38:47 +0800

    Update idMappingTb.

--- inst/extdata/build_idMappingTb.R
+++ inst/extdata/build_idMappingTb.R
@@ -5,4 +5,11 @@ datasets <- query( ah, "metaboliteIDmapping")
 data1 <- ah[["AH79817"]]
 data2 <- ah[["AH83115"]]
 data3 <- ah[["AH91792"]]
-saveRDS(data2, file = "./idMappingTb.rds")
+
+data2$ChEBI <- unname(sapply(data2$ChEBI, function(x) {
+  if(is.na(x)) return(NA)
+  else paste0("CHEBI:", x)
+}))
+
+saveRDS(data2, file = "./inst/extdata/idMappingTb.rds")
+
